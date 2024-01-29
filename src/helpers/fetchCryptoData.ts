ea96646f693fde5dcb902465700bdf71115bb2a5
Will <33244107+w3ye@users.noreply.github.com>
2024-01-29 18:40:38 -0500

    Add response type

--- src/helpers/fetchCryptoData.ts
+++ src/helpers/fetchCryptoData.ts
@@ -1,17 +1,15 @@
 import axios from "axios";
+import { LatestListingResponse } from "../types/response";
 
-export const fetchLatestCrypto = async () => {
-  const response = await axios.get(
-    "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-    {
-      params: {
-        start: 1,
-        limit: 10,
-      },
-      headers: {
-        "X-CMC_PRO_API_KEY": import.meta.env.VITE_COIN_MARKET_CAP_API_KEY,
-      },
-    }
-  );
+export const fetchLatestCrypto = async (): Promise<LatestListingResponse[]> => {
+  const response = await axios.get("/api/v1/cryptocurrency/listings/latest", {
+    params: {
+      start: 1,
+      limit: 10,
+    },
+    headers: {
+      "X-CMC_PRO_API_KEY": import.meta.env.VITE_COIN_MARKET_CAP_API_KEY,
+    },
+  });
   return response.data.data;
 };
