79349dc8824054fc29b6063b7d727a7919ec51d6
Will <33244107+w3ye@users.noreply.github.com>
2024-01-29 19:56:09 -0500

    implement styling for crypto rows

--- src/components/latestCryptoTable/LatestCryptoRows.tsx
+++ src/components/latestCryptoTable/LatestCryptoRows.tsx
@@ -1,4 +1,4 @@
-import { Table } from "@mantine/core";
+import { Flex, Table } from "@mantine/core";
 import { LatestListingResponse } from "../../types/response";
 
 type LatestCryptoRowsProps = {
@@ -8,15 +8,35 @@ export const LatestCryptoRows = ({ latestCrypto }: LatestCryptoRowsProps) => {
   const rows = latestCrypto.map((crypto) => {
     return (
       <Table.Tr key={crypto.id}>
-        <Table.Td>{crypto.name}</Table.Td>
+        <Table.Td>
+          <Flex direction="column">
+            <div style={{ fontWeight: "bold", fontSize: "16px" }}>
+              {crypto.name}
+            </div>
+            <div>{crypto.symbol}</div>
+          </Flex>
+        </Table.Td>
         <Table.Td>{crypto.quote.USD.price}</Table.Td>
-        <Table.Td>{crypto.quote.USD.market_cap.toFixed(2)}</Table.Td>
+        <Table.Td>$ {crypto.quote.USD.market_cap.toFixed(2)}</Table.Td>
         <Table.Td>{crypto.circulating_supply.toFixed(2)}</Table.Td>
         <Table.Td>
-          {(crypto.quote.USD.percent_change_90d * 100).toFixed(2)}
+          <div
+            style={{
+              color: crypto.quote.USD.percent_change_90d > 0 ? "green" : "red",
+            }}
+          >
+            {(crypto.quote.USD.percent_change_90d * 100).toFixed(2)}{" "}
+          </div>
         </Table.Td>
         <Table.Td>
-          {(crypto.quote.USD.percent_change_24h * 100).toFixed(2)}
+          <div
+            style={{
+              color:
+                crypto.quote.USD.percent_change_24h * 100 > 0 ? "green" : "red",
+            }}
+          >
+            {(crypto.quote.USD.percent_change_24h * 100).toFixed(2)}
+          </div>
         </Table.Td>
       </Table.Tr>
     );
--- src/components/latestCryptoTable/LatestCryptoTable.tsx
+++ src/components/latestCryptoTable/LatestCryptoTable.tsx
@@ -12,8 +12,8 @@ export const LatestCryptoTable = () => {
           <Table.Th>Price</Table.Th>
           <Table.Th>Market Cap</Table.Th>
           <Table.Th>Circulating Supply</Table.Th>
-          <Table.Th>Change %(90d)</Table.Th>
-          <Table.Th>Last(24H)</Table.Th>
+          <Table.Th>Change(90d) %</Table.Th>
+          <Table.Th>Last(24H) %</Table.Th>
         </Table.Tr>
       </Table.Thead>
       <LatestCryptoRows latestCrypto={latestCrypto} />
