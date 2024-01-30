3406c1e22e5f69f69dc596d00d2756d2d81f4c01
Will <33244107+w3ye@users.noreply.github.com>
2024-01-29 19:13:53 -0500

    Add package mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import "./App.css";
+import { Box, Flex, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 function App() {
-  return <></>;
+  return (
+    <MantineProvider>
+      <Box m="md">
+        <Flex gap="xs" direction="column">
+          <div>Hello world</div>
+        </Flex>
+      </Box>
+    </MantineProvider>
+  );
 }
 
 export default App;
